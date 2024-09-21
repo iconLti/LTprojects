5f7d6f49bbe3635f35ef569df3b843efb789b99b
iconLti <timloktionov.icon@gmail.com>
2024-09-21 19:48:45 +0300

    first variant of lab01

--- OofCom/Lab01/lab01.cpp
+++ OofCom/Lab01/lab01.cpp
@@ -3,9 +3,10 @@
 
 using namespace std;
 
-// В начале отчета:
-// 1. описание длины полей P & M float & double
-// 2. принцип хранения отрицательных значений float & double
+union FloatIntUnion {
+        float f;
+        unsigned int i;
+    };
 
 
 /*************************************************************************************************
@@ -13,10 +14,9 @@ using namespace std;
  * @param num Число типа unsigned int
  *************************************************************************************************/
 void printBinary(unsigned int num) {
-    // Цикл от 31 до 0 (по позициям битов)
     for (int i = ((sizeof(unsigned int) * 8) - 1); i >= 0; i--) {
-        unsigned int musk = 1 << i; // Создаём маску для проверки конкретного бита
-        cout << ((num & musk) ? '1' : '0'); // Выводим 1, если бит установлен, иначе 0
+        unsigned int mask = 1 << i;
+        cout << ((num & mask) ? '1' : '0');
     }
     cout << endl;
 }
@@ -26,26 +26,46 @@ void printBinary(unsigned int num) {
  * @param num Число типа float
  *************************************************************************************************/
 void printBinary(float num) {
-    // Используем union для работы с float как с unsigned int
-    union {
-        float f;
-        unsigned int i;
-    } u;
-    u.f = num; // Сохраняем число float в union
+    FloatIntUnion u;
+    u.f = num;
 
-    // Цикл для вывода 32 бит числа float как unsigned int
     for (int i = ((sizeof(float) * 8) - 1); i >= 0; i--) {
-        unsigned int mask = 1 << i; // Маска для проверки конкретного бита
-        cout << ((u.i & mask) ? '1' : '0'); // Выводим 1, если бит установлен, иначе 0
+        unsigned int mask = 1 << i;
+        cout << ((u.i & mask) ? '1' : '0');
     }
     cout << endl;
 }
 
+/*************************************************************************************************
+ * @brief Выполняет зеркальную перестановку битов в группе
+ * @param num Число типа unsigned int
+ * @param k Количество бит в группе
+ * @param l Номер младшего бита в группе
+ * @return Число после перестановки
+ *************************************************************************************************/
+unsigned int reverseBitsInGroup(unsigned int num, int k, int l) {
+    unsigned int group = (num >> l) & ((1 << k) - 1); // Извлекаем группу битов
+    unsigned int reversedGroup = 0;
+
+
+    // Выполняем реверс группы битов
+    for (int i = 0; i < k; i++) {
+        if (group & (1 << i)) {
+            reversedGroup |= (1 << (k - 1 - i));
+        }
+    }
+
+    // Очищаем старую группу битов в числе и вставляем новую
+    num &= ~(((1 << k) - 1) << l); // Очищаем группу
+    num |= (reversedGroup << l);   // Вставляем реверсированную группу
+
+    return num;
+}
 
 int main() {
     unsigned int uIntNum;
     float floatNum;
-
+    int k, l;
 
     // Ввод unsigned int числа
     cout << "Enter an unsigned int number: ";
@@ -63,18 +83,50 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    
-    // Вывод в двоичной форме и десятичной форме:
-    cout << "Binary representation of unsigned int: ";
+
+    // Вывод исходных значений в двоичной форме и десятичной формах
+    cout << "\nOriginal binary representation of unsigned int: ";
     printBinary(uIntNum);
-    cout << "Decimal representation of unsigned int: ";
-    cout << uIntNum << endl;
+    cout << "Original decimal representation of unsigned int: " << uIntNum << endl;
 
-    cout << "Binary representation of float: ";
+    cout << "Original binary representation of float: ";
     printBinary(floatNum);
-    cout << "Decimal representation of float: ";
-    cout << floatNum << endl;
+    cout << "Original decimal representation of float: " << floatNum << endl;
+    cout << endl;
 
+    // Ввод параметров для зеркальной перестановки
+    cout << "Enter the number of bits in the group (k): ";
+    while (!(cin >> k) || k <= 0 || k > 32) {
+        cout << "Invalid input. Please enter a valid number of bits (1-32): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << "Enter the position of the least significant bit in the group (l): ";
+    while (!(cin >> l) || l < 0 || l > (32 - k)) {
+        cout << "Invalid input. Please enter a valid bit position (0-" << (32 - k) << "): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+
+    // Перестановка битов в unsigned int числе
+    unsigned int modifiedUInt = reverseBitsInGroup(uIntNum, k, l);
+
+    // Перестановка битов в float числе (работаем с его битовым представлением)
+    FloatIntUnion u;
+    u.f = floatNum;
+    u.i = reverseBitsInGroup(u.i, k, l);
+    float modifiedFloat = u.f;
+
+    // Вывод модифицированных значений в двоичной форме и десятичной форме
+    cout << "\nModified binary representation of unsigned int: ";
+    printBinary(modifiedUInt);
+    cout << "Modified decimal representation of unsigned int: " << modifiedUInt << endl;
+
+    cout << "Modified binary representation of float: ";
+    printBinary(modifiedFloat);
+    cout << "Modified decimal representation of float: " << modifiedFloat << endl;
 
     return 0;
 }
--- OofCom/Lab01/testLab01.cpp
+++ OofCom/Lab01/testLab01.cpp
@@ -1,96 +1,73 @@
 #include <iostream>
-#include <bitset>
 #include <limits>
 
 using namespace std;
 
-
-/**************************************************************************************************
- * @brief Отображает число типа unsigned int в двоичной форме.
- * 
- * Функция использует объект std::bitset для вывода 32-битного представления числа.
- * 
- * @param num Число типа unsigned int для отображения.
- ************************************************************************************************/
-void printBinary(unsigned int num) {
-    bitset<32> bits(num);
-    cout << "Binary representation of unsigned int: " << bits << endl;
-}
-
-
-/**************************************************************************************************
- * @brief Отображает число типа float в двоичной форме.
- * 
- * Для преобразования числа float в его двоичное представление используется union,
- * позволяющий работать с числом как с типом unsigned int.
- * 
- * @param num Число типа float для отображения.
-*************************************************************************************************/
-void printBinary(float num) {
-    union {
+union FloatIntUnion {
         float f;
         unsigned int i;
-    } u;
-    u.f = num;
-    bitset<32> bits(u.i);
-    cout << "Binary representation of float: " << bits << endl;
+    };
+
+
+/*************************************************************************************************
+ * @brief Выводит число в виде 32-битного двоичного представления
+ * @param num Число типа unsigned int
+ *************************************************************************************************/
+void printBinary(unsigned int num) {
+    for (int i = ((sizeof(unsigned int) * 8) - 1); i >= 0; i--) {
+        unsigned int mask = 1 << i;
+        cout << ((num & mask) ? '1' : '0');
+    }
+    cout << endl;
 }
 
+/*************************************************************************************************
+ * @brief Выводит число типа float в виде 32-битного двоичного представления
+ * @param num Число типа float
+ *************************************************************************************************/
+void printBinary(float num) {
+    FloatIntUnion u;
+    u.f = num;
 
-/**************************************************************************************************
- * @brief Зеркально переставляет группу рядом стоящих разрядов числа.
- * 
- * Функция принимает число, начальную позицию и количество битов, которые необходимо перевернуть.
- * Она извлекает биты, зеркально их переставляет, и вставляет обратно в исходное число.
- * 
- * @param num Число типа unsigned int, в котором нужно переставить биты.
- * @param start Позиция (начиная с 0) младшего бита группы, которую нужно переставить.
- * @param count Количество битов, которые необходимо перевернуть.
- * 
- * @return unsigned int Новое число с зеркально переставленной группой битов.
-*************************************************************************************************/
-unsigned int reverseBits(unsigned int num, int start, int count) {
-    // Создаем маску для выделения нужной группы битов
-    unsigned int mask = ((1 << count) - 1) << start;
-    
-    // Извлекаем биты из числа, используя маску
-    unsigned int group = (num & mask) >> start;
-    
-    // Переменная для хранения перевернутой группы битов
+    for (int i = ((sizeof(float) * 8) - 1); i >= 0; i--) {
+        unsigned int mask = 1 << i;
+        cout << ((u.i & mask) ? '1' : '0');
+    }
+    cout << endl;
+}
+
+/*************************************************************************************************
+ * @brief Выполняет зеркальную перестановку битов в группе
+ * @param num Число типа unsigned int
+ * @param k Количество бит в группе
+ * @param l Номер младшего бита в группе
+ * @return Число после перестановки
+ *************************************************************************************************/
+unsigned int reverseBitsInGroup(unsigned int num, int k, int l) {
+    unsigned int group = (num >> l) & ((1 << k) - 1); // Извлекаем группу битов
     unsigned int reversedGroup = 0;
 
-    // Зеркально переставляем биты в группе
-    for (int i = 0; i < count; ++i) {
-        // Извлекаем бит на позиции i и вставляем его на соответствующую зеркальную позицию
-        reversedGroup |= ((group >> i) & 1) << (count - i - 1);
+
+    // Выполняем реверс группы битов
+    for (int i = 0; i < k; i++) {
+        if (group & (1 << i)) {
+            reversedGroup |= (1 << (k - 1 - i));
+        }
     }
 
-    // Очищаем старую группу битов в исходном числе
-    num &= ~mask;
-    
-    // Вставляем перевернутую группу битов обратно в исходное число
-    num |= (reversedGroup << start);
-    
-    // Возвращаем результат с переставленными битами
+    // Очищаем старую группу битов в числе и вставляем новую
+    num &= ~(((1 << k) - 1) << l); // Очищаем группу
+    num |= (reversedGroup << l);   // Вставляем реверсированную группу
+
     return num;
 }
 
-
-/**************************************************************************************************
- * @brief Основная функция программы.
- * 
- * Программа запрашивает у пользователя ввод числа типа unsigned int и float, 
- * отображает их в двоичном виде,
- * затем запрашивает начальную позицию и количество битов для зеркальной перестановки и выводит
- * результат в двоичном формате.
- * 
- * @return int Возвращает 0 в случае успешного выполнения программы.
-*************************************************************************************************/
 int main() {
     unsigned int uIntNum;
     float floatNum;
-    
-    // Ввод данных для unsigned int с проверкой корректности
+    int k, l;
+
+    // Ввод unsigned int числа
     cout << "Enter an unsigned int number: ";
     while (!(cin >> uIntNum)) {
         cout << "Invalid input. Please enter an unsigned int: ";
@@ -98,7 +75,7 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    // Ввод данных для float с проверкой корректности
+    // Ввод float числа
     cout << "Enter a float number: ";
     while (!(cin >> floatNum)) {
         cout << "Invalid input. Please enter a float number: ";
@@ -106,23 +83,50 @@ int main() {
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    // Вывод чисел в двоичной форме
-    printBinary(uIntNum);
-    printBinary(floatNum);
-    
-    // Ввод параметров для зеркальной перестановки разрядов с проверкой на корректность
-    int start, count;
-    do {
-        cout << "Enter the starting bit position (0-31) for unsigned int: ";
-        cin >> start;
-        cout << "Enter the number of bits to reverse: ";
-        cin >> count;
-    } while (start < 0 || start > 31 || count <= 0 || (start + count) > 32);
 
-    // Зеркальная перестановка битов и вывод результата
-    unsigned int reversedUInt = reverseBits(uIntNum, start, count);
-    cout << "Original unsigned int: " << bitset<32>(uIntNum) << endl;
-    cout << "Reversed unsigned int: " << bitset<32>(reversedUInt) << endl;
+    // Вывод исходных значений в двоичной форме и десятичной формах
+    cout << "\nOriginal binary representation of unsigned int: ";
+    printBinary(uIntNum);
+    cout << "Original decimal representation of unsigned int: " << uIntNum << endl;
+
+    cout << "Original binary representation of float: ";
+    printBinary(floatNum);
+    cout << "Original decimal representation of float: " << floatNum << endl;
+    cout << endl;
+
+    // Ввод параметров для зеркальной перестановки
+    cout << "Enter the number of bits in the group (k): ";
+    while (!(cin >> k) || k <= 0 || k > 32) {
+        cout << "Invalid input. Please enter a valid number of bits (1-32): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    cout << "Enter the position of the least significant bit in the group (l): ";
+    while (!(cin >> l) || l < 0 || l > (32 - k)) {
+        cout << "Invalid input. Please enter a valid bit position (0-" << (32 - k) << "): ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+
+    // Перестановка битов в unsigned int числе
+    unsigned int modifiedUInt = reverseBitsInGroup(uIntNum, k, l);
+
+    // Перестановка битов в float числе (работаем с его битовым представлением)
+    FloatIntUnion u;
+    u.f = floatNum;
+    u.i = reverseBitsInGroup(u.i, k, l);
+    float modifiedFloat = u.f;
+
+    // Вывод модифицированных значений в двоичной форме и десятичной форме
+    cout << "\nModified binary representation of unsigned int: ";
+    printBinary(modifiedUInt);
+    cout << "Modified decimal representation of unsigned int: " << modifiedUInt << endl;
+
+    cout << "Modified binary representation of float: ";
+    printBinary(modifiedFloat);
+    cout << "Modified decimal representation of float: " << modifiedFloat << endl;
 
     return 0;
 }
